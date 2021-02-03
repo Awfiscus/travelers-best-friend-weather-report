35c877e676bd11d829bf600eed3d7d3c2de63511
Drew Weston <awfiscus@comcast.net>
2021-02-02 17:24:08 -0700

    added functionality to five day forcast that loads data including symbols for the next five days

--- script.js
+++ script.js
@@ -2,23 +2,19 @@
 var cities = []
 var searchBtn = $("#searchBtn")
 var displayDay = moment().format("L")
-var fiveDay = ["0", "1", "2", "3", "4", "5"]
+var fiveDay = ["0", "#1st", "#2nd", "#3rd", "#4th", "#5th"]
 var lat = ""
 var long = ""
 
 console.log(displayDay);
 
 
-searchBtn.on("click", function() {
+searchBtn.on("click", function city() {
     var inputData = $("#searchBar").val()
     cities.push(inputData)
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + inputData + "&cnt=6&units=imperial&appid=f75ae5425b4709812bd7285990411889"
     
-
-
-    // queryURL for UV index
-    //"https://api.openweathermap.org/data/2.5/onecall?" + inputData + "&exlude=current,minutely,hourly,alerts&units=imperial&appid=f75ae5425b4709812bd7285990411889"
 
     $.ajax({
         url: queryURL,
@@ -28,20 +24,25 @@ searchBtn.on("click", function() {
         console.log(response.list);
         var results = response.list;
         
-        //save longitude and latitude to variables for second ajax call to rretireve UV index
-
+        //save longitude and latitude to variables for second ajax call to retireve UV index
+        var iconCode = results[0].weather[0].icon
+        console.log(iconCode);
+        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
 
         var mainTitle = $("#mainTitle")
-        mainTitle.text(response.city.name + " " + displayDay + " " + results[0].weather.icon)
+        mainTitle.text(response.city.name + " " + displayDay)
+        $("#iconTitle").attr("src", iconUrl)
         $("#mainTemp").text(results[0].temp.day + " °F")
         $("#mainHum").text(results[0].humidity + "%")
         $("#mainWind").text(results[0].speed + " MPH")
-        $("mainUV")
         
         for(var i = 1; i < results.length; i++) {
-            fiveDay[i].attr($("h6").text(displayDay));
-            var temp = fiveDay[i].temp.day
-            fiveDay[i].attr($(".class").text(temp))
+           var iconCode = results[i].weather[0].icon
+           var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+           $(fiveDay[i]).children(".temp").text(results[i].temp.day + " °F")
+           $(fiveDay[i]).children(".humidity").text(results[i].humidity + "%")
+           $(fiveDay[i]).children(".symbol").attr("src", iconUrl)
+
         }
 
 
@@ -49,8 +50,11 @@ searchBtn.on("click", function() {
     })
 })
 
+
 //make a function for second ajax call to retrieve UV index
 
+    // queryURL for UV index
+    //"https://api.openweathermap.org/data/2.5/onecall?" + inputData + "&exlude=current,minutely,hourly,alerts&units=imperial&appid=f75ae5425b4709812bd7285990411889"
 
 
 searchBtn.on("click", function() {
@@ -59,3 +63,5 @@ searchBtn.on("click", function() {
     newLi.text($("#searchBar").val())
     $("#citiesList").append(newLi)
 })
+
+//make a function to get the locale stored value and set it to event listener to the created button
